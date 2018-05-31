8c3e1eaed95424d39e31b6fded5d7c64d942fda7
Ryan Kyle <rkyle@meredith.com>
2018-05-31 15:49:55 -0700

    Refactor createCreeps

--- createCreeps.js
+++ createCreeps.js
@@ -1,13 +1,13 @@
 let createCreeps = function(allCreeps) {
 
     const parts = {
-        harvester: [WORK, WORK, CARRY, MOVE],
-        upgrader: [WORK, CARRY, MOVE],
-        builder: [WORK, WORK, CARRY, MOVE],
+        harvester: [WORK, WORK, CARRY, MOVE], // 300
+        upgrader: [WORK, CARRY, MOVE], // 200
+        builder: [WORK, WORK, CARRY, MOVE], // 300
     };
 
     const maxCounts = {
-        harvester: 5,
+        harvester: 3,
         upgrader: 2,
         builder: 2,
     }
@@ -46,15 +46,23 @@ let createCreeps = function(allCreeps) {
     let builders = allCreeps.filter( (t) => {
         return t.creep.memory.role === 'builder'});
 
-    console.log("harvester count: " + harvesters.length);
-    console.log("upgrader count: " + upgraders.length);
+    // if (harvesters.length < maxCounts["harvester"]) {
+    //     spawnCreep("harvester");
+    // } else if (upgraders.length < maxCounts["upgrader"]) {
+    //     spawnCreep("upgrader");
+    // } else if (builders.length < maxCounts["builder"]) {
+    //     spawnCreep("builder");
+    // }
 
-    if (harvesters.length < maxCounts["harvester"]) {
-        spawnCreep("harvester");
-    } else if (upgraders.length < maxCounts["upgrader"]) {
-        spawnCreep("upgrader");
-    } else if (builders.length < maxCounts["builder"]) {
-        spawnCreep("builder");
+    let roles = ["harvester", "upgrader", "builder"];
+    for (let i in roles) {
+        let role = roles[i];
+        let things = allCreeps.filter((t) => t.creep.memory.role === role);
+        console.log(role + " count: " + things.length);
+        if (things.length < maxCounts[role]) {
+            spawnCreep(role);
+            break;
+        }
     }
 };
 
