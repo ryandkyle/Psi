ec644951c8c5a2ae6e0ef350e3c684b48f849905
David Badillo <dbadillo@meredith.com>
2018-05-31 17:22:04 -0700

    Fixing harvest and upgrade symbols for upgrader

--- role.upgrader.js
+++ role.upgrader.js
@@ -4,11 +4,11 @@ var roleUpgrader = {
     run: function(creep) {
         if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
-            creep.say('?? harvest');
+            creep.say('🔄 harvest');
         }
         if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
             creep.memory.upgrading = true;
-            creep.say('? upgrade');
+            creep.say('⚡ upgrade');
         }
 
         if(creep.memory.upgrading) {
