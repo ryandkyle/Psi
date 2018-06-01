e5a31602147f5825a944f2474a66c4b809511459
Ryan Kyle <rkyle@meredith.com>
2018-05-31 23:11:59 -0700

    Harvester testing. Unbuilt road limit

--- role.harvester.js
+++ role.harvester.js
@@ -5,34 +5,44 @@ let roleHarvester = {
 
         let getSourceLocation = function(sources) {
             let index = Math.floor(Math.random() * sources.length);
+            console.log("source index: " + index + " / " + sources.length);
             if (index > sources.length - 1) {
                 console.log("Index out of range trying to find a source. Random number failed us somehow. Falling back to 0.");
                 index = 0;
             }
+
             return sources[index];
         };
 
         // If less than full of energy
         if (creep.carry.energy < creep.carryCapacity) {
             let source = creep.memory.source;
-            //console.log(creep.name + "'s source is " + source);
 
-            if (source === undefined || source.id === undefined) {
+            if (source === undefined) {
                 //console.log(creep.name + "'s source was null");
                 let sources = creep.room.find(FIND_SOURCES);
+
+                console.log("sources found: " + sources.length);
                 source = getSourceLocation(sources);
+                //source = sources[0];
                 creep.memory.source = source;
                 console.log(creep.name + " found " + source.id);
             }
-            //console.log(creep.name + "'s source is now " + source.name);
+            //console.log(creep.name + "'s source is now " + source.id);
 
             // Harvest or move to Source
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                //console.log(creep.name + " is moving to " + source.name + " to harvest");
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            let errorCode = creep.harvest(creep.memory.source);
+            if (errorCode == ERR_NOT_IN_RANGE) {
+                console.log(creep.name + " is moving to " + creep.memory.source.id + " to harvest");
+                creep.moveTo(creep.memory.source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+            else {
+                console.log("error code: " + errorCode);
+                console.log("energy: " + creep.carry.energy);
             }
         }
         else {
+            console.log("full of energy or sumthin");
             // Transfer to Storage or move to Storage.
             let targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
@@ -41,10 +51,20 @@ let roleHarvester = {
                         structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
             });
+
+            let unbuiltRoads = creep.room.find(FIND_MY_CONSTRUCTION_SITES, {
+                filter: (site) => {
+                    return site.structureType == STRUCTURE_ROAD;
+                }
+            });
+
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    creep.room.createConstructionSite(creep.pos, STRUCTURE_ROAD, undefined);
+                    if (unbuiltRoads.length < 5) {
+                        console.log("Placing a road");
+                        creep.room.createConstructionSite(creep.pos, STRUCTURE_ROAD, undefined);
+                    }
                 }
             } else {
                 // Go home if you can't harvest or build.
